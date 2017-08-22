493ce752d802b7adf608cf833e24a724c0b497de
Renee MacDonald <rm51@users.noreply.github.com>
2017-08-21 22:39:34 -0700

    augmented images by flipping them

--- clone.py
+++ clone.py
@@ -19,8 +19,15 @@ for line in lines:
 	measurement = float(line[3])
 	measurements.append(measurement)
 
-X_train = np.array(images)
-y_train = np.array(measurements)
+augmented_images, augmented_measurements = [], []
+for image, measurement in zip(images, measurements):
+	augmented_images.append(image)
+	augmented_measurements.append(measurement)
+	augmented_images.append(cv2.flip(image,1))
+	augmented_measurements.append(measurement*-1.0)
+
+X_train = np.array(augmented_images)
+y_train = np.array(augmented_measurements)
 
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda
@@ -29,7 +36,6 @@ from keras.layers.pooling  import MaxPooling2D
 
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
-#model.add(Flatten(input_shape=(160,320,3)))
 model.add(Convolution2D(6,(5,5),activation="relu"))
 model.add(MaxPooling2D())
 model.add(Convolution2D(6,(5,5),activation="relu"))
