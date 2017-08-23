dc412999e7220bac0556f1a717bd04a24e11479e
Renee MacDonald <rm51@users.noreply.github.com>
2017-08-22 21:08:25 -0700

    Using all three cameras
    Gets further around the track, but stuck on the corner

--- clone.py
+++ clone.py
@@ -11,13 +11,14 @@ with open('data/driving_log.csv') as csvfile:
 images = []
 measurements = []
 for line in lines:
-	source_path = line[0]
-	filename = source_path.split('/')[-1]
-	current_path = 'data/IMG/' + filename
-	image = cv2.imread(current_path)
-	images.append(image)
-	measurement = float(line[3])
-	measurements.append(measurement)
+	for i in range(3):
+		source_path = line[i]
+		filename = source_path.split('/')[-1]
+		current_path = 'data/IMG/' + filename
+		image = cv2.imread(current_path)
+		images.append(image)
+		measurement = float(line[3])
+		measurements.append(measurement)
 
 augmented_images, augmented_measurements = [], []
 for image, measurement in zip(images, measurements):
