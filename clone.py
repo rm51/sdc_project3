4a8544b340256e90e84671754414fa09d398a4a9
Renee MacDonald <rm51@users.noreply.github.com>
2017-08-28 17:51:09 -0400

    Went on dirt path

--- clone.py
+++ clone.py
@@ -3,7 +3,7 @@ import cv2
 import numpy as np
 
 lines = []
-with open('withjoystick/driving_log.csv') as csvfile:
+with open('aug28/driving_log.csv') as csvfile:
 	reader = csv.reader(csvfile)
 	for line in reader:
 		lines.append(line)
@@ -14,7 +14,7 @@ for line in lines:
 	for i in range(3):
 		source_path = line[i]
 		filename = source_path.split('/')[-1]
-		current_path = 'withjoystick/IMG/' + filename
+		current_path = 'aug28/IMG/' + filename
 		image = cv2.imread(current_path)
 		images.append(image)
 		measurement = float(line[3])
@@ -24,8 +24,10 @@ augmented_images, augmented_measurements = [], []
 for image, measurement in zip(images, measurements):
 	augmented_images.append(image)
 	augmented_measurements.append(measurement)
-	augmented_images.append(cv2.flip(image,1))
-	augmented_measurements.append(measurement*-1.0)
+	flip_prob = np.random.random()
+	if flip_prob > 0.5:
+		augmented_images.append(cv2.flip(image,1))
+		augmented_measurements.append(measurement*-1.0)
 
 X_train = np.array(augmented_images)
 y_train = np.array(augmented_measurements)
@@ -34,12 +36,14 @@ from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Cropping2D
 from keras.layers import Convolution2D
 from keras.layers.pooling  import MaxPooling2D
+from keras.layers.core import Dropout
 
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
 model.add(Cropping2D(cropping=((50,20),(0,0))))
 model.add(Convolution2D(24,(5,5), strides=(2,2),activation="relu"))
 model.add(Convolution2D(36,(5,5),strides=(2,2),activation="relu"))
+model.add(Dropout(0.5))
 model.add(Convolution2D(48,(5,5),strides=(2,2),activation="relu"))
 model.add(Convolution2D(64,(3,3), activation="relu"))
 model.add(Convolution2D(64,(3,3),activation="relu"))
@@ -52,6 +56,6 @@ model.add(Dense(1))
 model.compile(loss='mse', optimizer='adam')
 model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=5)
 
-model.save('model.h5')
+model.save('modelaug28a.h5')
 exit(0)
 
