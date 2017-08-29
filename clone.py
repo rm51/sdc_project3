b4355cd4ef66b90c1ec2f8256d23e646415719e0
Renee MacDonald <rm51@users.noreply.github.com>
2017-08-29 11:39:58 -0400

    Model goes off dirt path or hits the corner
    And doesn't turn completely. To do record more of that turn

--- clone.py
+++ clone.py
@@ -2,6 +2,10 @@ import csv
 import cv2
 import numpy as np
 
+def resize(data):
+	from keras.backend import tf as ktf
+	return ktf.image.resize_images(data, (64, 64))
+
 lines = []
 with open('aug28/driving_log.csv') as csvfile:
 	reader = csv.reader(csvfile)
@@ -9,6 +13,8 @@ with open('aug28/driving_log.csv') as csvfile:
 		lines.append(line)
 
 images = []
+
+
 measurements = []
 for line in lines:
 	for i in range(3):
@@ -16,21 +22,27 @@ for line in lines:
 		filename = source_path.split('/')[-1]
 		current_path = 'aug28/IMG/' + filename
 		image = cv2.imread(current_path)
-		images.append(image)
 		measurement = float(line[3])
+		images.append(image)
 		measurements.append(measurement)
+
+#images = resize(images)
 
 augmented_images, augmented_measurements = [], []
 for image, measurement in zip(images, measurements):
 	augmented_images.append(image)
 	augmented_measurements.append(measurement)
 	flip_prob = np.random.random()
-	if flip_prob > 0.7:
-		augmented_images.append(cv2.flip(image,1))
-		augmented_measurements.append(measurement*-1.0)
+	# change flip_prob maybe make lower so more images flipped
+	#if flip_prob > 0.5:
+	#  when flipping only if measurement greater than .3 for turning result is worse
+	augmented_images.append(cv2.flip(image,1))
+	augmented_measurements.append(measurement*-1.0)
 
 X_train = np.array(augmented_images)
 y_train = np.array(augmented_measurements)
+
+
 
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Cropping2D
@@ -43,11 +55,12 @@ import matplotlib.pyplot as plt
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
 model.add(Cropping2D(cropping=((50,20),(0,0))))
+model.add(Lambda(resize))
 model.add(Convolution2D(24,(5,5), strides=(2,2),activation="relu"))
 model.add(Convolution2D(36,(5,5),strides=(2,2),activation="relu"))
-model.add(Dropout(0.5))
+#model.add(Dropout(0.5))
 model.add(Convolution2D(48,(5,5),strides=(2,2),activation="relu"))
-model.add(Convolution2D(64,(3,3), activation="relu"))
+model.add(Convolution2D(64,(3,3),activation="relu"))
 model.add(Convolution2D(64,(3,3),activation="relu"))
 model.add(Flatten())
 model.add(Dense(100))
